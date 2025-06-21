29db9087afab4413047ebc4080a875f89939bd96
baifamily <1371197610@qq.com>
2025-06-21 10:34:51 +0800

    add Working with Multiple Chat Models example

---
+++ spring-ai-alibaba-chat-example/multi-chat/src/main/java/com/alibaba/cloud/ai/example/chat/multi/MultipleChatModelsApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.cloud.ai.example.chat.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author baifamily
+ */
+
+@SpringBootApplication
+public class MultipleChatModelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultipleChatModelsApplication.class, args);
+    }
+}
---
+++ spring-ai-alibaba-chat-example/multi-chat/src/main/java/com/alibaba/cloud/ai/example/chat/multi/controller/MultiChatClientController.java
@@ -0,0 +1,97 @@
+package com.alibaba.cloud.ai.example.chat.multi.controller;
+
+
+import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.DefaultChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.deepseek.DeepSeekChatModel;
+import org.springframework.ai.deepseek.DeepSeekChatOptions;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author baifamily
+ */
+
+@RestController
+@RequestMapping
+public class MultiChatClientController {
+
+    private static final String DEFAULT_PROMPT = "你好，介绍下你自己！";
+
+    private static final String ASSISTANT_CHAT_PROMPT = "你是一名CEO助理";
+
+    private final ChatClient defaultChatClient;
+
+    private final ChatClient assistantChatClient;
+
+    private final ChatClient deepseekChatClient;
+
+    public MultiChatClientController(@Qualifier("dashscopeChatModel")ChatModel dashscopeChatModel,@Qualifier("deepSeekChatModel")ChatModel deepSeekChatModel) {
+        this.defaultChatClient = ChatClient.builder(dashscopeChatModel)
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor()
+                )
+                .defaultOptions(
+                        DashScopeChatOptions.builder()
+                                .withMaxToken(1000)
+                                .build()
+                )
+                .build();
+
+        this.assistantChatClient = ChatClient.builder(dashscopeChatModel)
+                .defaultSystem(ASSISTANT_CHAT_PROMPT)
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor()
+                )
+                .defaultOptions(
+                        DashScopeChatOptions.builder()
+                                .withMaxToken(1000)
+                                .build()
+                )
+                .build();
+
+        this.deepseekChatClient = ChatClient.builder(deepSeekChatModel)
+                .defaultAdvisors(
+                        new SimpleLoggerAdvisor()
+                )
+                .defaultOptions(
+                        DeepSeekChatOptions.builder()
+                                .topP(0.7)
+                                .build()
+                )
+                .build();
+    }
+
+    /**
+     * 利用单个ChatModel创建多个ChatClient的简单调用
+     * */
+    @GetMapping("/single/chat")
+    public String singleChat() {
+        StringBuilder s = new StringBuilder();
+        s.append("defaultChatClient:");
+        s.append(defaultChatClient.prompt(DEFAULT_PROMPT).call().content());
+        s.append(System.lineSeparator());
+        s.append("assistantChatClient:");
+        s.append(assistantChatClient.prompt(DEFAULT_PROMPT).call().content());
+        return s.toString();
+    }
+
+    /**
+     * 利用dashscopeChatModel和deepSeekChatModel两个模型分别创建ChatClient的简单调用
+     * */
+    @GetMapping("/multi/chat")
+    public String multiChat() {
+        StringBuilder s = new StringBuilder();
+        s.append("defaultChatClient:");
+        s.append(defaultChatClient.prompt(DEFAULT_PROMPT).call().content());
+        s.append(System.lineSeparator());
+        s.append("DeepSeekChatClient:");
+        s.append(deepseekChatClient.prompt(DEFAULT_PROMPT).call().content());
+        return s.toString();
+    }
+}
